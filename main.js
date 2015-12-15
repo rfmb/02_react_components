40f6d914343c8f18db78cfa7a8c23d4dee470e5a
swennemans <sven.roeterdink1@gmail.com>
2015-12-15 13:07:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/* Create our first ReactComponet ! */
+// const MyFirstComponent =
+
+
+
+const root = document.getElementById("app")
+ReactDOM.render(React.createElement(MyFirstComponent), root);
